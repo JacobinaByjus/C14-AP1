51e00e309b3572afe9854df7601fb7c1d226bec0
JacobinaByjus <jacobinalarissa@gmail.com>
2021-09-13 17:36:33 -0300

    vs2

--- sketch.js
+++ sketch.js
@@ -1,22 +1,19 @@
 var Student = {
   name: "Sammy",
   class: 7,
-  roll_no:21,
-  favorite_subject: "programação",
-  marks : [30,35,40,50]
-  
+  registrationNo: 21,
+  favoriteSubject: "programação",
+  marks: [30, 35, 40, 50],
 };
 
 function setup() {
-  
   createCanvas(400, 400);
   //mudandoo o número da matrícula
-  console.log(Student.roll_no);
-  Student.roll_no = 45;
-  console.log(Student.roll_no);  
+  console.log(Student.registrationNo);
+  Student.registrationNo = 45;
+  console.log(Student.registrationNo);
 }
 
 function draw() {
   background(220);
-  
 }
